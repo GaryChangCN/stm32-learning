8f4d964e9b8a71ba00b3a81cb5ad50941e74cee0
= <=>
2023-06-10 01:10:37 +0800

    init

--- User/main.c
+++ User/main.c
@@ -2,31 +2,17 @@
 
 
 int main(void) {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStruct);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	GPIO_InitTypeDef GPIO_initStruct;
 
-	// GPIO_SetBits(GPIOC, GPIO_Pin_13);  // 不亮
-	// GPIO_ResetBits(GPIOC, GPIO_Pin_13);  // 亮
-	while (1)
-	{
- 
-		// delay(2000);
-		
+	GPIO_initStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_initStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_initStruct.GPIO_Speed = GPIO_Speed_50MHz;
 
-		// delay(2000);
+	GPIO_Init(GPIOA, &GPIO_initStruct);
+
+	GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+
+	while(1) {
 	}
-	
- 
-
-
-	// RCC->APB2ENR = 0x00000010;
-	// GPIOC->CRH = 0x00300000;
-	// GPIOC->ODR = 0x00002000;
-
-	// while(1) {
-	// }
 }
